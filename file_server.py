5e16522b846a4e91f08b2dca0f1132cd3bb06efd
Clive Micallef <sniper_pro18@hotmail.com>
2019-08-21 20:38:13 +0200

    Todo app Changes

--- file_server.py
+++ file_server.py
@@ -71,7 +71,7 @@ def return_data():
     start_date = request.args.get('start', '')
     end_date = request.args.get('end', '')
 
-    with open("events.json", "r") as input_data:
+    with open("data/events.json", "r") as input_data:
         return input_data.read()
 
 ##End Calendar
